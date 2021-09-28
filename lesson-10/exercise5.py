75999522151b79f8b08355a72acfb48b7c0afab4
aiale88@gmail.com <aia.rupsom@interacso.com>
2021-09-28 18:11:33 +0300

    Exercises 4-6 with map and filter funcions

--- lesson-10/exercise5.py
+++ lesson-10/exercise5.py
@@ -1,2 +1,6 @@
 result = [i * 100 if i % 2 == 0 else i for i in range(1,10)]
 print(result)
+
+# option 2
+result2 = list(map(lambda x: x * 100 if x % 2 == 0 else x, range(1,10)))
+print(result2)
--- lesson-10/exercise6.py
+++ lesson-10/exercise6.py
@@ -1,2 +1,6 @@
 result = ['Boom' if i % 7 == 0 else i for i in range(1,100)]
 print(result)
+
+# option 2
+result2 = list(map(lambda x: 'Boom' if x % 7 == 0 else x, range(1,100)))
+print(result2)
