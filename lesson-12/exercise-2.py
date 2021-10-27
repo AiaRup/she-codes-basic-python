ad97119ae3fd91c4f52c8241a84908d93c787166
Aia Rupsom <aiale88@gmail.com>
2021-10-27 19:30:30 +0300

    Add exercise 2 lesson 12- print_top function

--- lesson-12/exercise-2.py
+++ lesson-12/exercise-2.py
@@ -20,4 +20,15 @@ def count_word(full_text):
   return result
 
 
-print_words("./lesson-12/alice.txt")
+def print_top(file_name):
+  with open(file_name, "r") as text_file:
+    data = text_file. read()
+    result = count_word(data)
+    sorted_result = [{k: v} for k, v in sorted(result.items(), key=lambda item: item[1], reverse=True)]
+    for i in range(20):
+      for item in sorted_result[i]:
+        print(f'{item} {sorted_result[i][item]}')
+
+
+# print_words("./lesson-12/alice.txt")
+print_top("./lesson-12/alice.txt")
