4d27a39abcc6d5854d4e8c5cccf043f1eddff092
Aia Rupsom <aiale88@gmail.com>
2021-10-30 17:07:36 +0300

    Finish MyAgent and HomanAgent for the game

--- lesson-11.1/agent.py
+++ lesson-11.1/agent.py
@@ -6,42 +6,54 @@ author: Sven Schmit
 
 import random
 
+
 class Agent:
-    def getMove(self, moves_other, moves_self):
-        pass
+  def getMove(self, moves_other, moves_self):
+    pass
 
 
 class InstructorAgent(Agent):
-    def __init__(self):
-        p_rock = random.random()
-        p_scissors = random.random()
-        p_paper = random.random()
-        p_total = p_rock + p_scissors + p_paper
+  def __init__(self):
+    p_rock = random.random()
+    p_scissors = random.random()
+    p_paper = random.random()
+    p_total = p_rock + p_scissors + p_paper
 
-        self.p_rock = p_rock / p_total
-        self.p_scissors = self.p_rock + p_scissors / p_total
+    self.p_rock = p_rock / p_total
+    self.p_scissors = self.p_rock + p_scissors / p_total
 
-    def getMove(self, moves_other, moves_self):
-        random_move = random.random()
-        if random_move < self.p_rock:
-            return 'r'
-        elif random_move < self.p_scissors:
-            return 's'
-        else:
-            return 'p'
+  def getMove(self, moves_other, moves_self):
+    random_move = random.random()
+    if random_move < self.p_rock:
+      return 'r'
+    elif random_move < self.p_scissors:
+      return 's'
+    else:
+      return 'p'
 
 
 class MyAgent(Agent):
-    def getMove(self, moves_other, moves_self):
+  def getMove(self, moves_other, moves_self):
+    def __init__(self):
+      p_rock = 100
+      p_scissors = random.random()
+      p_paper = 20
+      p_total = p_rock + p_scissors + p_paper
 
-        # YOUR CODE HERE
-
-        pass
+      self.p_rock = p_rock / p_total
+      self.p_scissors = p_rock + p_scissors / p_total
+    random_move = random.random()
+    if random_move < self.p_rock and moves_other[0] != 'r':
+      return 'r'
+    elif random_move < self.p_scissors and moves_self[len(moves_self) - 1] != 'p':
+      return 's'
+    else:
+      return 'p'
 
 
 class HumanAgent(Agent):
-    def getMove(self, moves_other, moves_self):
-
-        # YOUR CODE HERE
-
-        pass
+  def getMove(self, moves_other, moves_self):
+    step = ''
+    while step != 'r' and step != 'p' and step != 's':
+      step = input('What is your next move? Please enter one letter: \nr for ROCK\ns for SCISSORS\np for PAPER\n=>')
+    return step
--- lesson-11.1/game.py
+++ lesson-11.1/game.py
@@ -3,71 +3,71 @@ Rock-Paper-Scissors game
 Starter code for Stanford CME 193
 author: Sven Schmit
 '''
+from collections import Counter
+
 
 class Game:
-    def __init__(self, agent1, agent2):
-        self.moves_a1 = []
-        self.moves_a2 = []
-        self.nround = 0
-        self.score_a1 = 0
-        self.score_a2 = 0
+  def __init__(self, agent1, agent2):
+    self.moves_a1 = []
+    self.moves_a2 = []
+    self.nround = 0
+    self.score_a1 = 0
+    self.score_a2 = 0
 
-        self.agent1 = agent1
-        self.agent2 = agent2
+    self.agent1 = agent1
+    self.agent2 = agent2
 
-    def round(self):
-        print('Round {}: '.format(self.nround+1))
-        # we pass along history so a player can take that into account when
-        # deciding the next move
-        move_a1 = self.agent1.getMove(self.moves_a2, self.moves_a1)
-        move_a2 = self.agent2.getMove(self.moves_a1, self.moves_a2)
+  def round(self):
+    print('Round {}: '.format(self.nround+1))
+    # we pass along history so a player can take that into account when
+    # deciding the next move
+    move_a1 = self.agent1.getMove(self.moves_a2, self.moves_a1)
+    move_a2 = self.agent2.getMove(self.moves_a1, self.moves_a2)
 
-        # compare the moves and decide who wins
-        outcome = self.compare(move_a1, move_a2)
+    # compare the moves and decide who wins
+    outcome = self.compare(move_a1, move_a2)
 
-        # output outcome of current round
-        if outcome == 1:
-            print('Player 1 wins: {} beats {}'.format(move_a1, move_a2))
-        elif outcome == 0:
-            print('This round is tied, both players played {}'.format(move_a1))
-        else:
-            print('Player 2 wins: {} beats {}'.format(move_a2, move_a1))
+    # output outcome of current round
+    if outcome == 1:
+      print('Player 1 wins: {} beats {}'.format(move_a1, move_a2))
+      self.score_a1 += 1
+    elif outcome == 0:
+      print('This round is tied, both players played {}'.format(move_a1))
+    else:
+      print('Player 2 wins: {} beats {}'.format(move_a2, move_a1))
+      self.score_a2 += 1
 
-        # update scores
+    self.moves_a1.append(move_a1)
+    self.moves_a2.append(move_a2)
 
-        # YOUR CODE HERE
+  def play(self, nrounds):
+    for r in range(nrounds):
+      self.round()
+      self.nround += 1
 
-        self.moves_a1.append(move_a1)
-        self.moves_a2.append(move_a2)
+  def summary(self):
+    # print some summary of the games
+    print('='*20)
+    if self.score_a1 > self.score_a2:
+      print('Player 1 wins')
+    elif self.score_a1 < self.score_a2:
+      print('Player 2 wins')
+    else:
+      print('The game ends in a tie')
 
-    def play(self, nrounds):
-        for r in range(nrounds):
-            self.round()
-            self.nround += 1
+    print('Final score: {} - {}'.format(self.score_a1, self.score_a2))
 
-    def summary(self):
-        # print some summary of the games
-        print('='*20)
-        if self.score_a1 > self.score_a2:
-            print('Player 1 wins')
-        elif self.score_a1 < self.score_a2:
-            print('Player 2 wins')
-        else:
-            print('The game ends in a tie')
+    all_moves = [*self.moves_a1, *self.moves_a2]
+    most_played_move = Counter(all_moves).most_common(1)[0][0]
 
-        print('Final score: {} - {}'.format(self.score_a1, self.score_a2))
+    print('Most played move: {}'.format(most_played_move))
+    print('='*20)
 
-        # To implement: find the move that was played most often
-
-        # YOUR CODE HERE
-
-        print('Most played move: {}'.format(0))
-        print('='*20)
-
-    def compare(self, move1, move2):
-        # returns 1 if move1 wins, 0 if it's a tie, and -1 if move2 wins
-
-        # YOUR CODE HERE
-
-        return 0
-
+  def compare(self, move1, move2):
+    # returns 1 if move1 wins, 0 if it's a tie, and -1 if move2 wins
+    if move1 == move2:
+      return 0
+    elif (move1 == 'r' and move2 == 's') or (move1 == 's' and move2 == 'p') or (move1 == 'p' and move2 == 'r'):
+      return 1
+    else:
+      return -1
